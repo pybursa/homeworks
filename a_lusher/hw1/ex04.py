39ff60cc64ce0d974630a5cf00f37e0029d645be
wowkalucky <wowkalucky@gmail.com>
2014-11-14 14:44:37 +0200

    Added a_lusher homeworks #1 - #3

---
+++ a_lusher/hw1/ex04.py
@@ -0,0 +1,21 @@
+﻿#!usr/bin/python
+
+#my_string=raw_input("Please enter a string: ")
+my_string="sabrrtuwacaddabra"
+
+
+def find_longest_substring_in_alphabetical_order(s):
+    groups = []
+    cur_longest = ''
+    prev_char = ''
+    for c in s.lower():
+        if prev_char and c < prev_char:
+            groups.append(cur_longest)
+            cur_longest = c
+        else:
+            cur_longest += c
+        prev_char = c
+    return max(groups, key=len)
+
+
+print find_longest_substring_in_alphabetical_order(my_string)
---
+++ a_lusher/hw3/Lusher_Alexander_home_work_3_.py
@@ -0,0 +1,90 @@
+﻿#!/usr/bin/python
+# -*- coding: utf-8 -*- 
+#
+
+def add(x, y):
+	a=1
+	while a>0:
+		a = x & y
+		b =  x ^ y
+		x = b
+		y = a << 1
+	return b
+
+def vowel_count(word):
+	vowels_counter = 0
+	for letter in word:
+        	if letter.isalpha():
+            		if letter.upper() in 'AEIOUY':
+                		vowels_counter += 1
+	return vowels_counter
+
+if __name__ == '__main__':
+
+	# Assignment N 1
+	text="Proin eget tortor risus. Cras ultricies ligula sed magna dictum porta. Proin eget tortor risus. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Donec rutrum congue leo eget malesuada."
+	
+	list=text.split()
+	max_vowel_number=0
+	for i in range(0,len(list)-1):
+		print "word=",list[i],"		number of vowels",vowel_count(list[i])	
+		if  vowel_count(list[i])>max_vowel_number:
+			max_vowel_number=vowel_count(list[i])
+	print "Maximum number of vowels is",max_vowel_number
+
+	# Assignment N 2
+
+	text="Proin eget tortor risus. Cras ultricies ligula sed magna dictum porta. Proin eget tortor risus. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Donec rutrum congue leo eget malesuada."
+	
+	list=text.split()
+	length=len(list[0])
+	words=[]
+	words.append(list[0])
+	for i in range(1,len(list)-1):
+		if length<len(list[i]):
+			length=len(list[i])
+			words[:] = []
+			words.append(list[i])
+		elif length==len(list[i]):
+			words.append(list[i])
+	print "maximum length=",length,"words are",words
+
+	# Assignment N 3
+	text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis lorem ut libero malesuada feugiat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec rutrum congue leo eget malesuada. Cras ultricies ligula sed magna dictum porta."	
+	list=text.split()
+	i=len(text)-1
+	mirrored_text=''
+	while i>=0:
+		mirrored_text=mirrored_text+(text[i])
+		i-=1
+	print mirrored_text
+
+	# Assignment N 4
+	import os
+	content=dir(os)
+	content_len=len(content)
+	for k in range(0,content_len-1):
+		s="os"+"."+content[k]+".__doc__"
+		print(eval(s))
+
+	import sys
+	content=dir(sys)
+	content_len=len(content)
+	for k in range(0,content_len-1):
+		s="sys"+"."+content[k]+".__doc__"
+		print(eval(s))
+
+
+	# Assignment N 5
+	input=12345
+
+	a=str(input)
+	str_len=len(a)
+	i=0
+	total=int(a[i])
+	while i<str_len-1:
+		total=add(total,int(a[add(i,1)]))
+		i=add(i,1)
+	print total
+	
+

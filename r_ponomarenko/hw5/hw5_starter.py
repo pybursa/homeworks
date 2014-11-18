36a445e29ccb513085c3603ecc3fb4f452b89a20
rm-ponomarenko <ponomarenko.roman@gmail.com>
2014-11-19 01:38:35 +0200

    Added files for hw5

---
+++ r_ponomarenko/hw5/hw5_starter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+u"""
+Основной скрипт запуска ДЗ.
+
+Данный скрипт призван запускать на выполнение домашнее задание #5.
+Также выполняется комплекс тестов из модуля набора тестов.
+"""
+
+__author__ = "wowkalucky"
+__email__ = "wowkalucky@gmail.com"
+__date__ = "2014-11-17"
+
+
+import hw5_solution1
+from hw5_tests import tests_for_hw5_solution1
+
+
+def runner():
+    u"""Запускает выполнение всех задач"""
+    # здесь можно запустить что-то
+    pass
+
+if __name__ == '__main__':
+    runner()
+    tests_for_hw5_solution1()

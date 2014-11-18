ef6fc116cfea2e6c4434ac3daefc2cd9900c1512
Шум Константин <k.shym@ya.ru>
2014-11-18 11:15:51 +0300

    ДЗ 5

---
+++ k_shum/hw5/hw5_starter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+u"""
+Основной скрипт запуска ДЗ.
+Данный скрипт призван запускать на выполнение домашнее задание #5.
+Также выполняется комплекс тестов из модуля набора тестов.
+"""
+
+__author__ = "k.shym"
+__email__ = "ks.shym@gmail.com"
+__date__ = "2014-11-18"
+
+
+import hw5_solution1
+from hw5_tests import tests_for_hw5_solution1
+
+
+def runner():
+    u"""Запускает выполнение всех задач"""
+    # здесь можно запустить что-то
+    pass
+
+if __name__ == '__main__':
+    runner()
+    tests_for_hw5_solution1()

6217313ee99420c746657b7e71ccaadfa7d70a4a
wowkalucky <wowkalucky@gmail.com>
2014-11-20 15:47:25 +0200

    Added m_zhelyaskoff hw5

---
+++ m_zhelyaskoff/hw5/hw5_starter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+u"""
+Основной скрипт запуска ДЗ.
+Данный скрипт призван запускать на выполнение домашнее задание #5.
+Также выполняется комплекс тестов из модуля набора тестов.
+"""
+
+__author__ = "wowkalucky"
+__email__ = "wowkalucky@gmail.com"
+__date__ = "2014-11-17"
+
+
+import hw5_solution1
+
+from hw5_tests import tests_for_hw5_solution1
+
+
+def runner():
+    u"""Запускает выполнение всех задач"""
+    pass
+
+if __name__ == '__main__':
+    runner()
+    tests_for_hw5_solution1()

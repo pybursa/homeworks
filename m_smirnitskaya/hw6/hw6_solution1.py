47439f1ecffb2b4b78ff901c6e59d2fa633237e4
mushmushroom <smirnitskayamp@gmail.com>
2014-11-21 23:15:00 +0200

    checked spaces

--- m_smirnitskaya/hw6/hw6_solution1.py
+++ m_smirnitskaya/hw6/hw6_solution1.py
@@ -9,7 +9,7 @@ from hw5_solution1 import Person
 
 def modifier(file):
     """
-    This function accepts a csv file and modifies it according to the task:
+     This function accepts a csv file and modifies it according to the task:
     creates Person classes, adds fullname and age columns
     """
     fp = open(file, "r")
@@ -29,6 +29,6 @@ def modifier(file):
     fw = open(file, "w")
     csv_writer = csv.writer(fw)
     for i in new:
-        csv_writer.writerow(i)
+       csv_writer.writerow(i)
 
 
